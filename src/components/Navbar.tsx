227082eedfbe07fa514cfe93f0d3d59c50e56466
Gregorio González <gregoryglez11@gmail.com>
2025-05-31 13:43:51 -0600

    feat: navbar component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 interface NavbarProps {
-  title: string;
+  title: string | React.ReactNode;
   ctaText: string;
   onCtaClick: () => void;
 }
@@ -12,8 +12,26 @@ export const Navbar: React.FC<NavbarProps> = ({
   onCtaClick,
 }) => {
   return (
-    <nav className="bg-gray-800 text-white p-4">
-      <h1 className="text-2xl">My Application</h1>
+    <nav className="navbar">
+      <div className="navbar-brand">
+        <span className="navbar-item">{title}</span>
+        <button className="button is-primary" onClick={onCtaClick}>
+          {ctaText}
+        </button>
+        <h1 className="">My Application</h1>
+      </div>
+
+      <div className="navbar-menu">
+        <div className="navbar-start">
+          <a className="navbar-item" href="#home">Inicio</a>
+          <a className="navbar-item" href="#about">Sobre nosotros</a>
+          <a className="navbar-item" href="#contact">Contactanos</a>
+        </div>
+        <div className="navbar-end">
+          <a className="navbar-item" href="#login">Login</a>
+          <a className="navbar-item" href="#signup">Sign Up</a>
+        </div>
+      </div>
     </nav>
   );
 }
